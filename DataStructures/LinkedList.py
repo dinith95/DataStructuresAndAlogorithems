43348c2d9d8972c4947005dab7be725583e54417
Manula Jayabodhi <manula.jayabodhi@gmail.com>
2025-04-20 16:19:15 +0530

    added pop first and get functions

--- DataStructures/LinkedList.py
+++ DataStructures/LinkedList.py
@@ -60,7 +60,39 @@ class LinkedList:
 
             current = current.next
 
+    def popFirst(self):
+        if self.length == 0:
+            return None
+
+        # if there is only one node in the linked list
+        if self.head == self.tail:
+            firstNode = self.head
+            self.tail = None
+            self.head = None
+            self.length -= 1
+            return firstNode
+
+        firstNode = self.head
+        self.head = firstNode.next  # set the head to the next node
+        firstNode.next = None
+        self.length -= 1
+        return firstNode
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+
+        current = self.head
+
+        for _ in range(index):
+            current = current.next
+
+        return current.value
+
+
 # add a node to the beginning of the linked list
+
+
     def prepend(self, value):
         new_node = Node(value)
 
@@ -81,5 +113,7 @@ list = LinkedList(5)
 list.append(10)
 list.append(15)
 list.prepend(1)
+print(f'item at index 2: {list.get(2)}')  # get the value at index 2
+print(f'item at index 3: {list.get(3)}')  # get the value at index 0
 # print(list.pop())  # call pop method and print the returned value
 list.printList()
