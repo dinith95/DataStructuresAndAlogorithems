91f271d1a3202d32cd2baf9354693da138e84d33
Dinith Jayabodhi <manula.jayabodhi@gmail.com>
2025-05-03 19:10:16 +0530

    added append and append tests

--- DemoApp1/DataStructureTests/LinkedListTests.cs
+++ DemoApp1/DataStructureTests/LinkedListTests.cs
@@ -22,4 +22,19 @@ public class LinkedListTests
         Assert.That(linkeList.Tail, Is.SameAs(linkeList.Head));
         Assert.That(linkeList.Head.Value, Is.EqualTo(5));
     }
+
+    [Test]
+    public void WhenAppend_ShouldReturnAsExpected()
+    {
+        var linkeList = new CustomLinkedList(5);
+        linkeList.Append(10);
+        linkeList.Append(15);
+
+        Assert.That(linkeList.Head, Is.Not.Null);
+        Assert.That(linkeList.Tail, Is.Not.Null);
+        Assert.That(linkeList.Tail, Is.Not.SameAs(linkeList.Head));
+        Assert.That(linkeList.Head.Value, Is.EqualTo(5));
+        Assert.That(linkeList.Tail.Value, Is.EqualTo(15));
+        Assert.That(linkeList.PrintList(), Is.EqualTo("5,10,15,"));
+    }
 }
--- DemoApp1/DataStructures/CustomLinkedList.cs
+++ DemoApp1/DataStructures/CustomLinkedList.cs
@@ -1,16 +1,62 @@
-﻿namespace DataStructures;
+﻿using System.Text;
 
-public record Node(int Value, Node Next);
+namespace DataStructures;
+
+public class Node
+{
+    public Node(int value, Node next)
+    {
+        Value = value;
+        Next = next;
+    }
+
+    public Node(int value)
+    {
+        Value = value;
+    }
+
+    public int Value { get; set; }
+
+    public Node Next { get; set; }
+}
+
+
 
 public class CustomLinkedList
 {
-    public Node Head { get; init; }
-    
-    public Node Tail { get; init; }
+    public Node Head { get; private set; }
+
+    public Node Tail { get; private set; }
 
     public CustomLinkedList(int value)
     {
-        Head = new Node(value, null);
+        Head = new Node(value);
         Tail = Head;
+    }
+
+    public void Append(int value)
+    {
+        if (Head == null)
+        {
+            Head = new Node(value);
+            Tail = Head;
+        }
+        else
+        {
+            Tail.Next = new Node(value);
+            Tail = Tail.Next;
+        }
+    }
+
+    public string PrintList()
+    {
+        var current = Head;
+        var result = new StringBuilder();
+        while (current != null)
+        {
+            result.Append(current.Value + ",");
+            current = current.Next;
+        }
+        return result.ToString();
     }
 }
